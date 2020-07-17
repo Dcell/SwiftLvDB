fc05a5cf33d99b29883e56345ee9909fbf735771
Dcell <765736477@qq.com>
2020-07-17 14:30:23 +0800

    update testunit

--- swift-lvdb-demo/swift-lvdb-demoTests/swift_lvdb_demoTests.swift
+++ swift-lvdb-demo/swift-lvdb-demoTests/swift_lvdb_demoTests.swift
@@ -10,8 +10,10 @@ import XCTest
 import SwiftLvDB
 
 class swift_lvdb_demoTests: XCTestCase {
+    var sldb:SwiftLvDB!
 
     override func setUpWithError() throws {
+        sldb = SwiftLvDB(subName: "sldb", 0)//ignore memory cache
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
@@ -34,35 +36,103 @@ class swift_lvdb_demoTests: XCTestCase {
     func testSaveInt() throws{
         let testValue = 1
         let testKey = "testSaveInt"
-        SwiftLvDB.sharedInstance.set(testValue, forKey: testKey)
-        XCTAssertFalse(testValue != SwiftLvDB.sharedInstance.integer(forKey: testKey))
+        sldb.set(testValue, forKey: testKey)
+        XCTAssertFalse(testValue != sldb.integer(forKey: testKey))
     }
     func testSaveFloat() throws{
         let testValue:Float = 1.0
         let testKey = "testSaveFloat"
-        SwiftLvDB.sharedInstance.set(testValue, forKey: testKey)
-        XCTAssertFalse(testValue != SwiftLvDB.sharedInstance.float(forKey: testKey))
+        sldb.set(testValue, forKey: testKey)
+        XCTAssertFalse(testValue != sldb.float(forKey: testKey))
     }
     
     func testSaveDouble() throws{
         let testValue:Double = 1.0
         let testKey = "testSaveDouble"
-        SwiftLvDB.sharedInstance.set(testValue, forKey: testKey)
-        XCTAssertFalse(testValue != SwiftLvDB.sharedInstance.double(forKey: testKey))
+        sldb.set(testValue, forKey: testKey)
+        XCTAssertFalse(testValue != sldb.double(forKey: testKey))
     }
     
     func testSaveBool() throws{
         let testValue:Bool = true
         let testKey = "testSaveBool"
-        SwiftLvDB.sharedInstance.set(testValue, forKey: testKey)
-        XCTAssertFalse(testValue != SwiftLvDB.sharedInstance.bool(forKey: testKey))
+        sldb.set(testValue, forKey: testKey)
+        XCTAssertFalse(testValue != sldb.bool(forKey: testKey))
     }
     
     func testSaveString() throws{
         let testValue:String = "hello"
         let testKey = "testSaveString"
-        SwiftLvDB.sharedInstance.set(testValue, forKey: testKey)
-        XCTAssertFalse(testValue != SwiftLvDB.sharedInstance.string(forKey: testKey))
+        sldb.set(testValue, forKey: testKey)
+        XCTAssertFalse(testValue != sldb.string(forKey: testKey))
+    }
+    
+    func testSaveCodeable() throws{
+        struct TestStruct:Codable,Equatable{
+            var a:Int
+            var b:String
+            
+            static func == (lhs: Self, rhs: Self) -> Bool{
+                return lhs.a == rhs.a && lhs.b == rhs.b
+            }
+        }
+        let testKey = "testSaveCodeable"
+        let testvalue = TestStruct(a: 1, b: "hello")
+        try sldb.set(testvalue, forKey: testKey)
+        XCTAssertFalse(testvalue != sldb.codeableObject(TestStruct.self, forKey: testKey))
+    }
+    
+    func testSaveList() throws{
+        let testValue:[Int] = [1,2,3,4,5]
+        let testKey = "testSaveList"
+        try sldb.set(testValue, forKey: testKey)
+        XCTAssertFalse(testValue != sldb.codeableObject([Int].self, forKey: testKey))
+    }
+    
+    func testSaveHashMap() throws{
+        var testValue:[String:Int] = [:]
+        testValue["1"] = 1
+        testValue["2"] = 2
+        
+        let testKey = "testSaveList"
+        try sldb.set(testValue, forKey: testKey)
+        XCTAssertFalse(testValue != sldb.codeableObject([String:Int].self, forKey: testKey))
+    }
+    
+    func testSaveInt100000Times() throws{
+        for index in 0...100000 {
+            let testValue = index
+            let testKey = "testSaveInt\(index)"
+            sldb.set(testValue, forKey: testKey)
+            XCTAssertFalse(testValue != sldb.integer(forKey: testKey))
+        }
+    }
+    
+    func testSaveString100000Times() throws{
+        for index in 0...100000 {
+            let testValue = "testString\(index)"
+            let testKey = "testSaveString\(index)"
+            sldb.set(testValue, forKey: testKey)
+            XCTAssertFalse(testValue != sldb.string(forKey: testKey))
+        }
+    }
+    
+    func testSaveCodeable100000Times() throws{
+        struct TestStruct:Codable,Equatable{
+            var a:Int
+            var b:String
+            
+            static func == (lhs: Self, rhs: Self) -> Bool{
+                return lhs.a == rhs.a && lhs.b == rhs.b
+            }
+        }
+        for index in 0...100000 {
+            let testValue = TestStruct(a: index, b: "index\(index)")
+            let testKey = "testSaveCodeable\(index)"
+                
+            try sldb.set(testValue, forKey: testKey)
+            XCTAssertFalse(testValue != sldb.codeableObject(TestStruct.self, forKey: testKey))
+        }
     }
 
 }
